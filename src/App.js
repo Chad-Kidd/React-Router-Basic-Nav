8b6f22b600df728f8cd6552f021f10797f3e09e7
chad kidd <ckiddwebdev@gmail.com>
2019-04-01 15:29:50 -0700

    routes and links

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 
 const App = () => (
   <div>
     <Navigation />
+
+    <Route exact exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
   </div>
 );
 
